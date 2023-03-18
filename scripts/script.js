65ef2db0cece1eb1b2acc946003b2962f411c06e
lyonbett <lyon.bett@student.moringaschool.com>
2023-03-18 18:45:15 +0300

    update project files

--- scripts/script.js
+++ scripts/script.js
@@ -2,33 +2,43 @@
 function mymarks(){
     let marks = parseInt(prompt("Please enter your marks between 0 and 100"));
 
+    var destination= document.getElementById("grade_answer")
+
     if (marks< 0 || marks>100) {
-        alert("Marks should be between 0 and 100")
+        destination.innerHTML="Marks should be between 0 and 100."
     } 
     else if (marks>79) {
-        alert("Your Grade is A.")
+        
+        destination.innerHTML="Your Grade is A."
+
     } 
     else if (marks>=60 && marks <=79) {
-        alert("Your Grade is B.")
+        destination.innerHTML="Your Grade is B."
+
     } 
     else if (marks>49 && marks <=59) {
-        alert("Your Grade is C.")
+        destination.innerHTML="Your Grade is C."
+
     }
     else if (marks>=40 && marks <=49) {
-        alert("Your Grade is D.")
+        destination.innerHTML="Your Grade is D."
+
     }
     else if (marks<40) {
-        alert("Your Grade is E.")
+        destination.innerHTML="Your Grade is E."
+
     }
     else {
-       alert("Invalid Input") 
+       destination.innerHTML="Invalid Input."
     }
 }
 
 //  Speed Detector
 function calculateSpeed(){
-    
+
 }
 // Net Salary Calculator
 
-
+function calculateSalary(){
+    
+}
